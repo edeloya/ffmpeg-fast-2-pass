6a3d37ce70ce94c8955f46731123e0d001781185
edeloya <54195989+edeloya@users.noreply.github.com>
2022-01-05 15:30:16 -0600

    Add files via upload

--- bitrates.py
+++ bitrates.py
@@ -1,10 +1,10 @@
 import os, subprocess, re, collections, pprint, ffmpeg, statistics
 from pymkv import MKVFile
 
-def bitrate(X):                                             #parse file BITRATE
+def bitrate(X):                                                 #parse file BITRATE
     return int(ffmpeg.probe(X)['format']['bit_rate']) // 1000
 
-def gather(ext):                                            #checks for EXT, creates tmp directory for working files
+def gather(ext):                                                #checks for EXT, creates tmp directory for working files
     dirlist.clear()
     for i in os.listdir():
         if i.endswith(ext):
@@ -19,13 +19,13 @@ os.chdir(pydir)
 original, dirlist, eps = [], [], []
 regx = {}
 filetype = 'mkv'
-top = 3                                                     #uses the top # of samples
+top = 3                                                         #uses the top # of samples
 
 tmpcheck()
 gather(filetype)
-original = dirlist[:]                                            #[:] Slice Operator
+original = dirlist[:]                                           #[:] Slice Operator
         
-for every in dirlist:                                         #splits into copies of 60s slices 
+for every in dirlist:                                           #splits into copies of 60s slices 
     mkv = MKVFile(every)
     mkv.split_duration(60)
     mkv.mux( 'tmp\\' + mkv.tracks[0].file_path )
@@ -35,21 +35,19 @@ gather(filetype)
 
 for file in dirlist:
     if re.findall(r'(S\d{2}E\d{2})', file):
-        regx[re.findall(r'(S\d{2}E\d{2})', file)[0]] = None #regx[file] = None
+        regx[re.findall(r'(S\d{2}E\d{2})', file)[0]] = None     #regx[file] = None
 
-for x in regx:                                              #for every ep S0xE0x,
-    for i in dirlist:                                          #for every shard '<filename>-0xx.ext',
-        if x in i:                                          #if ep S0xE0x is found in any temp, 
-            eps.append( (i, bitrate(i)) )                    #add em to working list for this loop
-            with open('bit.s', 'a') as file:
-                file.write(eps)
-    regx[x] = ( sorted(eps, key=lambda x: x[1]) [:-top] )   #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
-                                                                              #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
-                                                                              #file-name3.mp4, bitrate)   from above
-                                                                              #asc sorted by bitrate at [:-3]
+for x in regx:                                                  #for every ep S0xE0x,
+    for i in dirlist:                                           #for every shard '<filename>-0xx.ext',
+        if x in i:                                              #if ep S0xE0x is found in any temp, 
+            eps.append( (i, bitrate(i)) )                       #add em to working list for this loop
+    regx[x] = ( sorted(eps, key=lambda x: x[1]) [:-top] )       #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
+                                                                                  #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
+                                                                                  #file-name3.mp4, bitrate)   from above
+                                                                                  #asc sorted by bitrate at [:-3]
     for i in regx[x]:
         os.remove(i[0])    
-    eps.clear()                                             #clear the eps of temp from ep x in regx
+    eps.clear()                                                 #clear the eps of temp from ep x in regx
 
 tmpcheck()
 gather(filetype)
@@ -57,7 +55,7 @@ gather(filetype)
 for ep in dirlist:
     infile = ffmpeg.input(ep)
     infile.video.output(
-    'tmp\\'+ep,                                             #filename
+    'tmp\\'+ep,                                                 #filename
     vcodec='libx265',
     preset='slow',
     crf='22'
@@ -67,12 +65,12 @@ tmpcheck()
 os.chdir('tmp')
 gather(filetype)
 
-for key in regx:                                            #go through unique episode list from original dir
-    regx[key] = None                                          #reset values
+for key in regx:                                                #go through unique episode list from original dir
+    regx[key] = None                                            #reset values
     for file in dirlist:
         if key in file:
-            eps.append( bitrate(file) )                    #make a list of every S0XE0X episode temp' bitrate
-    regx[key] = int(statistics.mean(eps))+100               #avg bitrate for top 3 temp, for this episode
+            eps.append( bitrate(file) )                         #make a list of every S0XE0X episode temp' bitrate
+    regx[key] = int(statistics.mean(eps))+100                   #avg bitrate for top 3 temp, for this episode
     eps.clear()
 
 os.chdir(pydir)
