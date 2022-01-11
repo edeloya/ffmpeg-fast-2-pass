dc7a82f9ecb4a680f57bd402820139edfa9afe13
0 <0>
2022-01-11 14:58:53 -0600

    0

--- bitrates.py
+++ bitrates.py
@@ -17,10 +17,10 @@ def tmpcheck(x='tmp'):
 pydir = os.path.dirname(os.path.abspath(__name__))
 os.chdir(pydir)
 original, dirlist, eps = [], [], []
-regx = {}
 top = 3                                                                 #uses the top # of samples
 
 tmpcheck()
+tmpcheck('new')
 gather()                                                                #ls the directory
 original = dirlist[:]                                                   #[:] Slice Operator
         
@@ -28,63 +28,55 @@ for file in original:                                                   #splits 
     segment = str(
         'ffmpeg -n -hide_banner -loglevel quiet -stats -i "' + file + '" -map 0 -c copy -f segment -segment_time 60 -reset_timestamps 1 ".\\tmp\\%03d-' + file + '"'
     )
-    
+    os.system(segment)
     os.chdir('tmp')
     gather()
     
-    for seg in dirlist:
-                eps.append( (i, bitrate(i)) )                           #add em to working list for this loop
-        regx[x] = ( sorted(eps, key=lambda x: x[1]) [:-top] )           #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
-                                                                                          #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
-                                                                                          #file-name3.mp4, bitrate)   from above
-                                                                                          #asc sorted by bitrate at [:-3]
-        for i in regx[x]:
-            os.remove(i[0])    
-        eps.clear()                                                     #clear the eps of temp from ep x in regx
+    for i in dirlist:
+        eps.append( (i, bitrate(i)) )                                   #add em to working list for this loop
+    delet = ( sorted(eps, key=lambda x: x[1]) [:-top] )                 #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
+                                                                                      #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
+                                                                                      #file-name3.mp4, bitrate)   from above
+                                                                                      #asc sorted by bitrate at [:-3]
+    for i in delet:
+        os.remove(i)                                                    #remove all but the top X segments
+        eps.remove(i)
+        
+    gather()
+    
+    for ep in dirlist:
+        infile = ffmpeg.input(ep)
+        infile.video.output(
+        '..\\new\\'+ep,                                              #filename
+        vcodec='libx265',
+        preset='slow',
+        crf='22'
+        ).run(overwrite_output=None)
 
-tmpcheck()
-gather(filetype)
+    os.chdir('..\\new')
+    os.remove('..\\tmp')
+    gather()
+    
+    for i in dirlist:
+        eps.append( (i, bitrate(i)) )                                 #make a list of every S0XE0X episode temp' bitrate        
+        os.remove(i)
 
-for ep in dirlist:
-    infile = ffmpeg.input(ep)
-    infile.video.output(
-    'tmp\\'+ep,                                                         #filename
-    vcodec='libx265',
-    preset='slow',
-    crf='22'
-    ).run(overwrite_output=None)
+    nicebitrate = int(statistics.mean(eps))+100                           #avg bitrate for top 3 temp, for this episode
+    eps.clear()
+    
+    os.chdir('..')
 
-tmpcheck()
-os.chdir('tmp')
-gather(filetype)
-
-for key in regx:                                                        #go through unique episode list from original dir
-    regx[key] = None                                                    #reset values
-    for file in dirlist:
-        if key in file:
-            eps.append( bitrate(file) )                                 #make a list of every S0XE0X episode temp' bitrate
-    regx[key] = int(statistics.mean(eps))+100                           #avg bitrate for top 3 temp, for this episode
-
-os.chdir(pydir)
-gather(filetype)
-
-tmpcheck('new')
-
-for ep in regx:
     for i in original:
-        if ep in i:
-            s_bitrate = str(regx[ep])
-            pass1 = str(
-                'ffmpeg -n -hide_banner -loglevel quiet -stats -an -sn -i \"' + pydir + '\\' + i + '\" -c:v libx265 -b:v ' + s_bitrate + 'K -x265-params pass=1 -f null NUL'
-            )
-            pass2 = str(
-                'ffmpeg -n -hide_banner -loglevel quiet -stats -i \"' + pydir + '\\' + i + '\" -c:v libx265 -b:v ' + s_bitrate + 'K -x265-params pass=2 -c:a libopus -b:a 160k \"' + pydir + '\\new\\' + i + '\"'
-            )
-            print('\n\n\nBitrate for '+ i + ' is: ' + s_bitrate)
-            print('\nRunning with command:\n'+pass1+'\n\n')
-            os.system(pass1)
-            print('\nRunning with command:\n'+pass2+'\n\n')
-            os.system(pass2)
+        pass1 = str(
+            'ffmpeg -n -hide_banner -loglevel quiet -stats -an -sn -i "' + i + '" -c:v libx265 -b:v ' + nicebitrate + 'K -x265-params pass=1 -f null NUL'
+        )
+        pass2 = str(
+            'ffmpeg -n -hide_banner -loglevel quiet -stats -i "' + i + '" -c:v libx265 -b:v ' + nicebitrate + 'K -x265-params pass=2 -c:a libopus -b:a 160k ".\\new\\' + i + '"'
+        )
+        print('\n\n\nBitrate for '+ i + ' is: ' + nicebitrate + 'K')
+        print('\nRunning with command:\n'+pass1+'\n\n')
+        os.system(pass1)
+        print('\nRunning with command:\n'+pass2+'\n\n')
+        os.system(pass2)        
 
 os.system("pause")
-os.system('rmdir /q /s tmp')
