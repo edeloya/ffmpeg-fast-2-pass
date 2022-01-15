f28a0cf11969f07d274bf7cdeafe33dde107da72
edeloya <54195989+edeloya@users.noreply.github.com>
2022-01-14 19:05:40 -0600

    '-'

--- bitrates.py
+++ bitrates.py
@@ -1,4 +1,4 @@
-import os, shutil, ffmpeg, statistics, numpy
+import os, shutil, ffmpeg, statistics, numpy, pprint
 
 def bitrate(X):                                                             #parse file BITRATE
     return int(ffmpeg.probe(X)['format']['bit_rate']) // 1000
@@ -34,13 +34,13 @@ for file in original:                                                       #spl
     for i in dirlist:
         eps.append( (i, bitrate(i)) )                                       #add em to working list for this loop
     
-    delet = ( sorted(eps, key=lambda x: x[1]) [:-top] )                     #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
-                                                                                              #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
-                                                                                              #file-name3.mp4, bitrate)   from above
-                                                                                              #asc sorted by bitrate at [:-3]
-    for i in delet:
-        # os.remove(i[0])                                                     #remove all EXCEPT the top X segments
-        eps.remove(i)
+    # delet = ( sorted(eps, key=lambda x: x[1]) [:-top] )                     #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
+    #                                                                                           #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
+    #                                                                                           #file-name3.mp4, bitrate)   from above
+    #                                                                                           #asc sorted by bitrate at [:-3]
+    # for i in delet:
+    #     os.remove(i[0])                                                     #remove all EXCEPT the top X segments
+    #     eps.remove(i)
         
     # gather()
     # tmpcheck()
@@ -48,10 +48,10 @@ for file in original:                                                       #spl
     for ep in dirlist:
         infile = ffmpeg.input(ep)
         infile.video.output(
-        '.\\tmp\\'+ep,                                                     #filename
-        vcodec='libx265',
-        preset='slow',
-        crf='24'
+            '.\\tmp\\'+ep, 
+            vcodec='libx265', 
+            preset='slow', 
+            crf='24'
         ).run(overwrite_output=True)
 
     os.chdir('.\\tmp\\')
@@ -61,7 +61,7 @@ for file in original:                                                       #spl
     for i in dirlist:
         eps.append( (i, bitrate(i)) )                                       #make a list of every S0XE0X episode temp' bitrate
     
-    print(eps)    
+    pprint.pprint(eps)    
     maxrate = ( sorted(eps, key=lambda x: x[1]) [top:] )
     avgbitrate = str( int( statistics.mean( [b for e,b in eps] ) )+100 )   #avg bitrate for top x temp, for this episode
 
@@ -75,9 +75,9 @@ for file in original:                                                       #spl
     )
     print('\n\n\nBitrate for '+ i + ' is: ' + avgbitrate + 'K')
     print('\nRunning with command:\n'+pass1+'\n\n')
-    os.system(pass1)
+    # os.system(pass1)
     print('\nRunning with command:\n'+pass2+'\n\n')
-    os.system(pass2)        
-    shutil.rmtree(pydir + '\\tmp')
+    # os.system(pass2)        
+    # shutil.rmtree(pydir + '\\tmp')
 
 os.system("pause")
