f36e981d1d34aaf63daea8e15d29932944e23ce1
edeloya <54195989+edeloya@users.noreply.github.com>
2022-01-15 20:59:12 -0600

    '-'

--- bitrates.py
+++ bitrates.py
@@ -16,7 +16,7 @@ def tmpcheck(x='tmp'):
 pydir = os.path.dirname(os.path.abspath(__name__))
 os.chdir(pydir)
 original, dirlist, eps = [], [], []
-top = 5                                                                     #uses the top # of samples
+top = 4                                                                     #uses the top # of samples
 
 tmpcheck()
 tmpcheck('new')
@@ -24,35 +24,31 @@ gather()                                                                    #ls 
 original = dirlist[:]                                                       #[:] Slice Operator
         
 for file in original:                                                       #splits into copies of 60s slices
+    tmpcheck()
+    tmpcheck('new')
     segment = str(
         'ffmpeg -n -hide_banner -loglevel quiet -stats -i \"{}\" -map 0 -c copy -f segment -segment_time 60 -reset_timestamps 1 \".\\tmp\\%03d-{}\"'.format(file,file)
     )
     os.system(segment)
     os.chdir('tmp')
+    tmpcheck()
     gather()
-    
+
     for i in dirlist:
         eps.append( (i, bitrate(i)) )                                       #add em to working list for this loop
+
+    eps = sorted(eps, key=lambda x: x[1])
     
-    delet = ( sorted(eps, key=lambda x: x[1]) [:-top] )                     #regx['S0xE0x'] = (file-name1.mp4, bitrate,   sorted(eps, by key x[1])
-                                                                                              #file-name2.mp4, bitrate,   in this case x = ( i, bitrate(i) )
-                                                                                              #file-name3.mp4, bitrate)   from above
-                                                                                              #asc sorted by bitrate at [:-3]
-    # for i in delet:
-    #     os.remove(i[0])                                                     #remove all EXCEPT the top X segments
-    #     eps.remove(i)
-        
-    gather()
-    tmpcheck()
-    
-    # for ep in dirlist:
-    #     infile = ffmpeg.input(ep)
-    #     infile.video.output(
-    #         '.\\tmp\\'+ep, 
-    #         vcodec='libx265', 
-    #         preset='slow', 
-    #         crf='24'
-    #     ).run(overwrite_output=True)
+    for i in eps[:-top]:
+        os.remove(i[0])                                                     #remove all EXCEPT the top X segments
+        eps.remove(i)
+
+    for ep in eps:
+        mojostop__= str(
+        'ffmpeg -n -hide_banner -loglevel quiet -stats -an -sn -i \"{}\" -c:v libx265 -crf 24 -preset slow \".\\tmp\\{}\"'.format(ep[0],ep[0])
+    )
+        print('\nRunning with command:\n{}\n\n'.format(mojostop__))
+        os.system(mojostop__)
 
     os.chdir('.\\tmp\\')
     eps.clear()
@@ -60,18 +56,20 @@ for file in original:                                                       #spl
     
     for i in dirlist:
         eps.append( (i, bitrate(i)) )                                       #make a list of every S0XE0X episode temp' bitrate
+    eps = sorted(eps, key=lambda x: x[1])                                   #sort by size
     
-    avgbitrate = int( statistics.mean( [b for e,b in eps] ) )+100   #avg bitrate for top x temp, for this episode
-    maxrate = sorted(eps, key=lambda x: x[1])[-1][1]
+    minrate = eps[0][1]
+    maxrate = eps[-1][1]
     bufsize = round( maxrate / 500, 2 )
+    avgbitrate = statistics.mean( [b for e,b in eps] )   #avg bitrate for top x temp, for this episode
 
     os.chdir(pydir)
 
     pass1 = str(
-        'ffmpeg -n -hide_banner -loglevel quiet -stats -an -sn -i \"{}" -c:v libx265 -b:v {}K -maxrate {}K -bufsize {}M -x265-params pass=1 -f null NUL'.format(file, avgbitrate, maxrate, bufsize)
+        'ffmpeg -n -hide_banner -loglevel quiet -stats -an -sn -i \"{}" -c:v libx265 -b:v {}K -minrate {}K -maxrate {}K -bufsize {}M -x265-params pass=1 -f null NUL'.format(file, avgbitrate, minrate, maxrate, bufsize)
     )
     pass2 = str(
-        'ffmpeg -n -hide_banner -loglevel quiet -stats -i \"{}" -c:v libx265 -b:v {}K -maxrate {}K -bufsize {}M -x265-params pass=2 -c:a libopus -b:a 160k ".\\new\\{}"'.format(file, avgbitrate, maxrate, bufsize, file)
+        'ffmpeg -n -hide_banner -loglevel quiet -stats -i \"{}" -c:v libx265 -b:v {}K -minrate {}K -maxrate {}K -bufsize {}M -x265-params pass=2 -c:a libopus -b:a 160k ".\\new\\{}"'.format(file, avgbitrate, minrate, maxrate, bufsize, file)
     )
 
     print('\n\n\nBitrate for {} is: {}K'.format(file, avgbitrate))
