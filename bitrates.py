ccc12366239c2c0917d31aea9b274f4397ed4a61
edeloya <54195989+edeloya@users.noreply.github.com>
2022-01-16 13:01:46 -0600

    '-'

--- bitrates.py
+++ bitrates.py
@@ -1,4 +1,4 @@
-import os, shutil, ffmpeg, statistics, numpy, pprint
+import os, shutil, ffmpeg, statistics, numpy, pprint, datetime
 
 def bitrate(X):                                                             #parse file BITRATE
     return int(ffmpeg.probe(X)['format']['bit_rate']) // 1000
@@ -13,9 +13,14 @@ def tmpcheck(x='tmp'):
     if os.path.isdir(x) == False:
         os.mkdir(x)
 
+def log(string=''):
+    with open('{}\\{}'.format(pydir,'.log'), 'a') as logfile:
+        logfile.write('[{}]    {}\n\n'.format(datetime.datetime.now(),string))
+    return
+
 pydir = os.path.dirname(os.path.abspath(__name__))
-os.chdir(pydir)
 original, dirlist, eps = [], [], []
+os.chdir(pydir)
 top = 4                                                                     #uses the top # of samples
 
 gather()                                                                    #ls the directory
@@ -28,7 +33,7 @@ for file in original:
         segment = (
             'ffmpeg -n -hide_banner -loglevel quiet -stats -i \"{}\" -map 0 -c copy -f segment -segment_time 60 -reset_timestamps 1 \".\\tmp\\%03d-{}\"'.format(file,file)
         )
-        print('Splitting {}..'.format(file))
+        log('Splitting {}..'.format(file))
         os.system(segment)
         os.chdir('tmp')
         tmpcheck()
@@ -38,6 +43,7 @@ for file in original:
             eps.append( (i, bitrate(i)) )                                       #add em to working list for this loop
 
         eps = sorted(eps, key=lambda x: x[1])
+        log(pprint.pprint(eps))
 
         for i in eps[:-top]:
             os.remove(i[0])                                                     #remove all EXCEPT the top X segments
@@ -47,7 +53,7 @@ for file in original:
             mojostop__= str(
             'ffmpeg -n -hide_banner -loglevel quiet -stats -an -sn -i \"{}\" -c:v libx265 -crf 24 -preset slow \".\\tmp\\{}\"'.format(ep[0],ep[0])
         )
-            print('\nRunning with command:\n{}\n\n'.format(mojostop__))
+            log('Running with command:\n{}'.format(mojostop__))
             os.system(mojostop__)
 
         os.chdir('.\\tmp\\')
@@ -57,12 +63,14 @@ for file in original:
         for i in dirlist:
             eps.append( (i, bitrate(i)) )                                       #make a list of every S0XE0X episode temp' bitrate
 
-        eps = [b for a,b in sorted(eps, key=lambda x: x[1])]
+        eps = sorted(eps, key=lambda x: x[1])
+        bits = [b for a,b in eps]
+        log(pprint.pprint(eps))
 
-        minrate = eps[0]
-        maxrate = eps[-1]
+        minrate = bits[0]
+        maxrate = bits[-1]
         bufsize = round( maxrate / 500, 2 )
-        avgbitrate = statistics.mean( eps )   #avg bitrate for top x temp, for this episode
+        avgbitrate = statistics.mean( bits )   #avg bitrate for top x temp, for this episode
 
         os.chdir(pydir)
 
@@ -73,15 +81,16 @@ for file in original:
             'ffmpeg -n -hide_banner -loglevel quiet -stats -i \"{}" -c:v libx265 -b:v {}K -minrate {}K -maxrate {}K -bufsize {}M -x265-params pass=2 -c:a libopus -b:a 160k ".\\new\\{}"'.format(file, avgbitrate, minrate, maxrate, bufsize, file)
         )
 
-        print('\n\n\nBitrate for {} is: {}K'.format(file, avgbitrate))
+        log('Bitrate for {} is: {}K'.format(file, avgbitrate))
 
-        print('\nRunning with command:\n{}\n\n'.format(pass1))
+        log('Running with command:\n{}'.format(pass1))
         os.system(pass1)
 
-        print('\nRunning with command:\n{}\n\n'.format(pass2))
+        log('Running with command:\n{}'.format(pass2))
         os.system(pass2)
 
         eps.clear()
+        bits.clear()
         shutil.rmtree(pydir + '\\tmp\\.')
 
 os.system("pause")
